c13321217346482504556b4372269c017fdc1026
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-09-07 20:48:25 +0200

    const selected

--- src/components/user.js
+++ src/components/user.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function user() {
   return (
     <div>user</div>
--- src/components/users.js
+++ src/components/users.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import User from './user'
 
-function users() {
+function users({users,page}) {
+    const startIndex = (page - 1) * 10;
+    const selectedUsers = users.slice(startIndex, startIndex + 10)
   return (
     <div>users</div>
   )
