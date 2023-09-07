6d07ebdb839da2713694faa7bd17587e334337b5
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-09-07 20:18:55 +0200

    await fetch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchusers = async () => {
+      const res = await fetch("https://randomuser.me/api/?page=1&results=50&nat=us")
+      ;
+    };
+  });
 
-
-  return (
-    <div>
-Pagination App
-    </div>
-  );
+  return <div>Pagination App</div>;
 }
 
 export default App;
